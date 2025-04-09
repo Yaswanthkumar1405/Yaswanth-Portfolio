bd82b8506e1b4dbdf5d035acef042b4aac838a22
Yaswanth Kumar Chaganti <yaswanthkumarchaganti@gmail.com>
2025-04-09 18:39:24 -0500

    layout changes

--- main.js
+++ main.js
@@ -1,31 +1,5 @@
-// Smooth scrolling for anchor links
-$(document).ready(function () {
-    $("a").on("click", function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-
-            $("html, body").animate(
-                {
-                    scrollTop: $(hash).offset().top,
-                },
-                800,
-                function () {
-                    window.location.hash = hash;
-                }
-            );
-        }
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init();
 });
 
-// Activate current section in menu based on scroll position
-$(window).on('scroll', function() {
-    var scrollPos = $(document).scrollTop();
-    $('nav ul li a').each(function() {
-        var sectionOffset = $($(this).attr('href')).offset().top - 100;
-        if (scrollPos >= sectionOffset) {
-            $('nav ul li a').removeClass('active');
-            $(this).addClass('active');
-        }
-    });
-});
+//  Removed all the previous "learnMoreLinks" and "showLessLinks" code
